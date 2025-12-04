9508107e2ddfa44b03f46846864f8a267ab273fe
beatrizormond <beatriz.ormond@unifesp.br>
2025-12-04 01:23:44 -0300

    Update 1610.c

--- 1610.c
+++ 1610.c
@@ -2,104 +2,86 @@
 #include <stdlib.h>
 
 typedef struct nodo {
-    int V;
-    struct nodo *seg;
-};
+	int v;
+	struct nodo *seg;
+} nodo;
 
-typedef struct {
-    int V; //número de vértices
-    int 
+int ciclo;
+int cor[10010];
+nodo *adj[10010]; //cada posiC'C#o do vetor terC! uma lista encadeada
 
-} grafo;
-
-grafo * criaGrafo(int V){
-
-}
-
-int ExisteLoop(grafo *g){
-    int V[g->V];
-    int i;
-
-    for (i=0; i<g->V; i++){
-        V[i] = 0;
-    }
-
-    for(i = 0; i<g->V; i++){
-        if(V[i] == 0){
-            if(dfs(g, i, V)){
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-int dfs(grafo *g, int u, int V[]){
-    V[u] = 1;
-    int v;
-
-    for(v = 0; v < g->V; v++){
-        if (g->matriz[u][v] == 1){
-            
-            if(V[v] == 1){
-                return 1;
-            }
-            if(V[v] == 0){
-                if(dfs(g, v, V)){
-                    return 1;
-                }
-            }
-        }
-    }
-
-    V[u] = 2;
-    return 0;
+void AdicionaAresta(int a, int b) {
+	nodo *novo = malloc(sizeof(nodo));
+	novo->v = b;
+	novo->seg = adj[a];
+	adj[a] = novo;
 }
 
 
-int main(){
-    int t, n, m, a, b, i, j;
-    t = 100;
+void dfs(int u) {
+	cor[u] = 1; //cinza - visitando
+	int v;
+	nodo *p = adj[u];
+
+	while (p != NULL) {
+		int v = p->v;
+
+		if(cor[v] == 1) {
+			ciclo = 1;
+			return;
+		}
+
+		if (cor[v] == 0) {
+			dfs(v);
+			if(ciclo) {
+				return;
+			}
+		}
+
+		p = p->seg;
+	}
+
+	cor[u] = 2; //preto - visitado
+}
+
+
+int main() {
+	int t, n, m, a, b, i, j;
     
-    for(i=0; i<t; i++){
-        scanf("%d %d", &n, &m);
-        grafo *g = criaGrafo(n);
-        
-        if(n>= 2 && n<=10000 && m>=1 && m<=30000){
-            for(j=0; j<m; j++){
-                scanf("%d %d", &a, &b);
-                if((a>=1) && (b<=n || a!=b)){
-                        g->matriz[a-1][b-1] = 1;
-                }
-            }
-        }
-        
-        if(ExisteLoop(g) == 1){
-            printf("SIM\n");
-        }
-        else{
-            printf("NAO\n");
-        }
-    }
-    
-    return 0;
-}
+    scanf("%d", &t);
+
+	for(i=0; i<t; i++) {
+		scanf("%d %d", &n, &m);
+
+		for (j = 1; j<=n; j++) {
+			adj[j] = NULL;
+			cor[j] = 0;
+		}
+
+		ciclo = 0;
 
 
+		for(j=0; j<m; j++) {
+			scanf("%d %d", &a, &b);
+			AdicionaAresta(a, b);
+		}
 
-//Função para criar grafo usando matriz de adjacência
-grafo * criaGrafo(int V){
-    int i, j;
-    grafo *g = malloc(sizeof(grafo));
-    g->V = V;
+		for (j=1; j<=n; j++) {
+			if (cor[j] == 0) {
+				dfs(j);
+				if (ciclo) {
+					break;
+				}
+			}
+		}
+		if (ciclo) {
+			printf("SIM\n");
+		}
+		else {
+			printf("NAO\n");
+		}
 
-    g->matriz = malloc(V*sizeof(int*));
+	}
 
-    for(i = 0; i<V; i++){
-        g->matriz[i] = malloc(V*sizeof(int));
-        for (j=0; j<V; j++){
-            g->matriz[i][j] = 0;
-        }
-    }
-    return g;
+	return 0;
 }
